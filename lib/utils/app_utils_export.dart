eae0e61a809ce959c94d3982a1f0130f6d75fccf
Sarvarjon Rustamov <105982396+sarvarrustam@users.noreply.github.com>
2022-12-12 09:50:43 +0500

    first commit

---
+++ lib/utils/app_utils_export.dart
@@ -0,0 +1,5 @@
+export 'package:instagram_clone/utils/app_constants.dart';
+export 'package:instagram_clone/utils/app_theme.dart';
+export 'package:instagram_clone/utils/app_utils.dart';
+export 'package:instagram_clone/utils/app_setup.dart';
+export 'package:instagram_clone/utils/app_routes.dart';
